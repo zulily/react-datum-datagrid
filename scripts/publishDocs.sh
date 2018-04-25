90872312241e9212d1f8ec8dcad62fe16fd7c884
Bee Wilkerson <bwilkerson@zulily.com>
2018-04-25 08:43:36 -0700

    one more change to docs publishing

--- scripts/publishDocs.sh
+++ scripts/publishDocs.sh
@@ -4,8 +4,10 @@ set -x
 git checkout gh-pages
 git reset --hard origin/master
 grunt clean build
-git add docs/
+# docs/ is normally ignored, use the --force to add it anyway in this branch
+git add --force docs/
 git commit -am 'rebuild docs for gh-pages'
 git push --force origin gh-pages
+git reset --hard origin/master
 git checkout master
 grunt build
