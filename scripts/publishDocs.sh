c4b06122132cf9be0342f8a6f02d80504b1dc094
Bee Wilkerson <bwilkerson@zulily.com>
2018-04-25 08:36:28 -0700

    change to make scripts/publishDocs.sh more reliable

--- scripts/publishDocs.sh
+++ scripts/publishDocs.sh
@@ -3,10 +3,10 @@ set -e
 set -x
 
 git checkout gh-pages
-git pull origin master
-grunt build
+git reset --hard origin/master
+grunt clean build
 git add docs/
 git commit -am 'rebuild docs for gh-pages'
-git push origin gh-pages
+git push --force origin gh-pages
 git checkout master
 grunt build
